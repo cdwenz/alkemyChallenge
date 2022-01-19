5e77da8ce04c648edb0c106ff5aba47b2cb27b0a
unknown <cdwenz@gmail.com>
2022-01-18 21:22:21 -0300

    model user

---
+++ api/src/Models/category.js
@@ -0,0 +1,15 @@
+const { Datatypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('category', {
+        id: {
+            type: Datatypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        category: {
+            type: Datatypes.STRING,
+            allowNull: false,
+        }
+    })
+}
